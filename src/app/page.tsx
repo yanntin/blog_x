8c55dcd9d26e0211223e26bf8272924633a6165c
yanxinyu <yanxinyu01@corp.netease.com>
2025-04-30 21:02:38 +0800

    feat: init blog repo

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]" style={{ transform: "translateX(-40px)" }}>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-3xl font-bold text-center sm:text-left">
+          莫瑞埃克
+        </h1>
+        <div className="text-center sm:text-left leading-relaxed text-gray-800 dark:text-gray-200 font-[family-name:var(--font-geist-sans)]">
+          <p className="mb-2 ">
+            安卓研发，初级前端研发，以及一个不给自己设限的人。
+          </p>
+          <p className="">
+            目前隐于不知名大厂，这个博客都是我的一些看法，谈不上真知灼见，如果能给您一点启发就好了。
+          </p>
+        </div>
+        <div className="w-full h-[1px] bg-black/[.1] dark:bg-white/[.1]"></div>
+        <ol className="list-inside list-decimal text-base/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)] text-gray-500">
+          <li className="mb-2 tracking-[-.01em]">
+            <a href="https://www.baidu.com" target="_blank" rel="noopener noreferrer" className="hover:underline">
+              安卓: 一例activity多任务栈导致的页面退到后台的问题
+            </a>
+          </li>
+          <li className="tracking-[-.01em]">
+            那些在我收藏单里的宝藏播客
+          </li>
+        </ol>
+      </main>
+    </div>
+  );
+}
