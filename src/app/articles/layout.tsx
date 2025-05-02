370f310cc3f660134dfacb3bbb149e3a1203ec6c
yanxinyu <yanxinyu01@corp.netease.com>
2025-05-02 13:49:31 +0800

    feat: modify background color

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 dark:bg-zinc-950 bg-white text-gray-900 dark:text-zinc-200">
+        <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 dark:bg-zinc-950 text-gray-900 dark:text-zinc-200">
             <main className="max-w-[60ch] mx-auto w-full space-y-6">
                 {children}
             </main>
