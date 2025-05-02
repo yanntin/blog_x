0138e80656a626889d598db1770d6feddfb09cc5
yanxinyu <yanxinyu01@corp.netease.com>
2025-05-02 13:31:14 +0800

    feat: modify article css layout

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -11,7 +11,7 @@ type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
 
 const components = {
   h1: (props: HeadingProps) => (
-    <h1 className="font-medium pt-12 mb-0" {...props} />
+    <h1 className="font-medium pt-12 mb-3 dark:text-zinc-200 mt-8" {...props} />
   ),
   h2: (props: HeadingProps) => (
     <h2
