98a2e58574e3f8da9a92b87be1a66c3ee4998854
yanxinyu <yanxinyu01@corp.netease.com>
2025-05-02 12:55:43 +0800

    feat: articles layout created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import "./globals.css";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]" style={{ transform: "translateX(-40px)" }}>
